8418149e38cc7f4692e3e71831b0815f4f9c942e
Alexey Kruminsh <aleksej.kruminsh@gmail.com>
2026-06-10 00:45:05 +0300

    fix: force bootstrap database migrations during upgrade

--- deploy/update-crm.sh
+++ deploy/update-crm.sh
@@ -44,7 +44,7 @@ run_database_migration() {
   compose run --rm -T \
     -e DISABLE_DB_MIGRATIONS=true \
     -e DISABLE_CRON_JOBS_REGISTRATION=true \
-    server yarn database:migrate:prod
+    server yarn database:migrate:prod --force
 }
 
 read_env_value() {
